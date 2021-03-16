5413445bf85ff3f628b3a9097c717bf68f5636e5
rp99 <rinalds.parpucis@gmail.com>
2021-03-16 10:48:33 +0200

    Set site as single page docs-only

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,29 +7,16 @@ module.exports = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'cenos', // Usually your GitHub org/user name.
+  projectName: 'cenos-docs-antennas', // Usually your repo name.
   themeConfig: {
     navbar: {
       title: 'CENOS Antenna Design',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'Cenos Logo',
         src: 'img/logo.svg',
       },
-      items: [
-        {
-          to: 'docs/',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
-        },
-        {to: 'blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
+      items: [],
     },
     footer: {
       style: 'dark',
@@ -86,17 +73,13 @@ module.exports = {
       '@docusaurus/preset-classic',
       {
         docs: {
+          routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
             'https://github.com/facebook/docusaurus/edit/master/website/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
-        },
+        blog: false,
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
