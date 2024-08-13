00e727f9a445d8a113dbf62f8c89757edce8041a
Rainers Justs <108451556+rljCENOS@users.noreply.github.com>
2024-08-13 15:48:53 +0300

    CP-5996 add RF CAD simplification page (#62)
    * added cad simplification page to docs and js
    
    * Deleted bolds and italics, replaced images with higher quality images and centered them.
    
    ---------
    
    Co-authored-by: Viesturs

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    Geometry: ['geometry-overview', 'geometry-good-cad', 'geometry-creation', 'geometry-lumped-ports', 'geometry-roles', 'geometry-CENOS-meshing', 'geometry-meshing', 'geometry-tips'],
+    Geometry: ['geometry-overview', 'CAD-adjustment', 'geometry-good-cad', 'geometry-creation', 'geometry-lumped-ports', 'geometry-roles', 'geometry-CENOS-meshing', 'geometry-meshing', 'geometry-tips'],
     Physics: ['physics-overview', 'physics-boundaries', 'batch-running'],
     Results: ['results-overview', 'results-advanced'],
     Examples: ['example-ifa-antenna', 'example-patch', 'example-dipole', 'example-helical-antenna'],
